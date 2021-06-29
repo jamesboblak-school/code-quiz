86239b5df2232c91bd1cce08141ba13afafd1a52
jamesboblak <james@skepticalrecords.com>
2021-06-29 07:44:06 -0700

    Added buttons for answers and time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var score = 0;
+var timeLeft = 60;
 var questionsObj = {
     q00 : "What does HTML stand for?",
     q01 : "What does CSS stand for?",
@@ -25,3 +26,23 @@ var answersObj = {
     a09 : "Structured Query Language",
     a01 : "GNU’s Not Unix",
 };
+
+// function startGame {
+//     var start = document.getElementById("start-button");
+//     start.addEventListener(click, question1());  
+// }
+
+function timer() {
+    document.getElementById("time-button").innerHTML = timeLeft;
+    // console.log("timer test");
+}
+timer();
+
+function question1() {
+    document.getElementById("questionBox").innerHTML = questionsObj.q00;
+        // console.log("question test");
+    document.getElementById("ans-a").innerHTML = answersObj.a00;
+    document.getElementById("ans-b").innerHTML = answersObj.a01;
+    document.getElementById("ans-c").innerHTML = answersObj.a02;
+}
+question1();
